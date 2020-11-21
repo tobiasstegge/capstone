c2096927732218610e8c303bc9f21d15b989e84d
Tobias Steggemann <tobias.steggemann@t-online.de>
2020-11-21 21:11:55 +0100

    copied auth python file

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+from os import curdir
+from models import Customer, Order
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
+
 
 def create_app(test_config=None):
   # create and configure the app
@@ -10,7 +14,23 @@ def create_app(test_config=None):
 
   return app
 
-APP = create_app()
+app = create_app()
+
+## ROUTES
+
+@app.route('/')
+def get_test():
+  return "API Working"
+
+@app.route('/customers', methods=["GET"])
+def get_all_customers():
+
+  customers = Customer.query.all()
+
+  return jsonify ({
+    'success': True,
+    'customers': [customer.format() for customer in customers]
+  })
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,1 +1,91 @@
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
+from sqlalchemy.sql.schema import PrimaryKeyConstraint
+
+from sqlalchemy.sql.sqltypes import Date
+
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Model Customer
+    Defines Table Customer
+'''
+class Customer(db.Model):
+  __tablename__ = 'Costumers'
+
+  id = Column(Integer, primary_key=True)
+  first_name = Column(String)
+  last_name = Column(String)
+  address = Column(String)
+  date_of_birth = Column(Date)
+  recieve_newsletter = Column(bool)
+
+  orders = db.relationship("Order")
+
+# not complete yet
+  def format(self):
+    return {
+    'id': self.id,
+    'first_name': self.first_name,
+    'last_name': self.last_name}
+
+
+def insert(self):
+  db.session.add(self)
+  db.session.commit()
+
+def update():
+  db.session.commit()
+
+def delete(self):
+  db.session.delete(self)
+  db.session.commit()
+
+'''
+Model Customer
+    Defines Table Customer
+'''
+class Order(db.Model):
+  __tablename__ = 'Orders'
+
+  id = Column(Integer, primary_key=True)
+  manufacturer = Column(String)
+  name_long = Column(String)
+  name_short = Column(String)
+  molecules = Column(String)
+  price = Column(float) 
+
+'''
+CRUD functions for DB interaction
+  defines functions in model scope
+'''
+def insert(model):
+  db.session.add(model)
+  db.session.commit()
+
+def update():
+  db.session.commit()
+
+def delete(model):
+  db.session.delete(model)
+  db.session.commit()
+
+
 
