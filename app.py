ea5531796aa30af116f6cca1870d56d3bb61317e
Tobias Steggemann <tobias.steggemann@t-online.de>
2020-11-22 12:16:02 +0100

    Database Creating working locally

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from os import curdir
-from models import Customer, Order
+from models import Customer, Order, setup_db
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 
-
 def create_app(test_config=None):
-  # create and configure the app
   app = Flask(__name__)
-  CORS(app)
+  setup_db(app)
+  CORS(app, resources={r"/*": {"origins": "*"}})
 
   return app
 
@@ -31,6 +30,7 @@ def get_all_customers():
     'success': True,
     'customers': [customer.format() for customer in customers]
   })
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -4,22 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 from sqlalchemy.sql.schema import ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.sql.sqltypes import Boolean, Date, Numeric
+from flask_migrate import Migrate
 
 database_path = os.environ['DATABASE_URL']
-
 db = SQLAlchemy()
 
 '''
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
-
 def setup_db(app, database_path=database_path):
+    print("Setting up DB")
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
 
 
 '''
@@ -27,16 +26,16 @@ Model Customer
     Defines Table Customer
 '''
 class Customer(db.Model):
-  __tablename__ = 'costumer'
+  __tablename__ = 'customer'
 
   id = Column(Integer, primary_key=True)
-  first_name = Column(String)
-  last_name = Column(String)
-  address = Column(String)
+  first_name = Column(String(100))
+  last_name = Column(String(100))
+  address = Column(String(100))
   date_of_birth = Column(Date)
   recieve_newsletter = Column(Boolean)
 
-  orders = db.relationship("Order")
+  orders = db.relationship('Order', backref='customer')
 
 # not complete yet
   def format(self):
@@ -65,10 +64,10 @@ class Order(db.Model):
   __tablename__ = 'order'
 
   id = Column(Integer, primary_key=True)
-  manufacturer = Column(String)
-  name_long = Column(String)
-  name_short = Column(String)
-  molecules = Column(String)
+  manufacturer = Column(String(100))
+  name_long = Column(String(100))
+  name_short = Column(String(100))
+  molecules = Column(String(100))
   price = Column(Numeric) 
 
   customer_id = Column(Integer, ForeignKey('customer.id'))
