fe820c2cb8bc10d92589fe88595b6d33b148638c
Tobias Steggemann <tobias.steggemann@t-online.de>
2020-11-22 23:33:35 +0100

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 export AUTH0_DOMAIN='savestegge.eu.auth0.com'
 export API_AUDIENCE='https://capstone-stegge-test/'
 export ALGORITHMS='RS256'
+export DATABASE_URL=postgres://xdjuboiqcjzpyf:f86f771e9008941a5a517cb901fe7ee3c7a9a4f5724a736fdc39b9366ca6f37f@ec2-54-172-219-218.compute-1.amazonaws.com:5432/dbhdlq90k74rje
